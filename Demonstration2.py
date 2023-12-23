2e564cefd6958d47474112d0772050e47a198a96
Perdixky <3293789706@qq.com>
2023-12-23 11:37:17 +0800

    改进代码2，现在可以自动确定颜色变化范围

--- Demonstration2.py
+++ Demonstration2.py
@@ -1,8 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-k = float(input("Enter the value of k: "))
-m = float(input("Enter the value of m: "))
+# 获取 k 和 m 的值
+k = float(input("Enter the value of k: ")) 
+m = float(input("Enter the value of k: "))
+
 # 定义方程组
 def system(Y):
     y, v = Y
@@ -11,7 +13,7 @@ def system(Y):
 # 创建网格
 Y, V = np.meshgrid(np.linspace(-3, 3, 20), np.linspace(-3, 3, 20))
 
-# 计算在网格上每一点的导数
+# 在每个网格点计算导数
 U, W = np.zeros(Y.shape), np.zeros(V.shape)
 for i in range(len(Y)):
     for j in range(len(V)):
@@ -24,10 +26,10 @@ for i in range(len(Y)):
 # 规范化箭头
 N = np.sqrt(U**2 + W**2)
 U, W = U/N, W/N
-N = N / np.max(N)  # 将箭头大小标准化到0-1之间
 
+# 自动计算颜色变化范围
 # 绘制方向场
-plt.quiver(Y, V, U, W, N, angles='xy', cmap=plt.cm.viridis, scale=35)  # 增加scale值以缩短箭头长度
+plt.quiver(Y, V, U, W, N, angles='xy', cmap=plt.cm.viridis, scale=35)  # Keeping scale to shorten arrow length
 plt.xlabel('x')
 plt.ylabel('v')
 plt.title(f'Colored Direction Field for a + {k}/{m} x = 0')
